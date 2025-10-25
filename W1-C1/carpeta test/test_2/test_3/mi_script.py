22d3b4627d3f0e8b0d86eda9a06126bc752088ea
anfagudelogo3 <anfagudelogo@gmail.com>
2025-10-25 07:49:51 -0500

    arqwe

---
+++ W1-C1/carpeta test/test_2/test_3/mi_script.py
@@ -0,0 +1,12 @@
+# Crea un archivo llamado mi_script.py en Visual Studio Code con este código
+
+# Un programa sencillo para sumar dos números
+def sumar(a, b):
+    return a + b
+
+# Entrada del usuario
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Muestra el resultado
+print(f"La suma de {num1} y {num2} es {sumar(num1, num2)}")
